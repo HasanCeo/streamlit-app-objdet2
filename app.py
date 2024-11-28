9f550b6e4b89d769c3bb340b08d021a5cd232ebd
HasanCeo <125748600+HasanCeo@users.noreply.github.com>
2024-11-28 13:12:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,24 @@ from ultralytics import YOLO
 
 st.title("YOLO Nesne Tespiti Uygulaması")
 
-@st.cache_resource  
-def load_model():
-    return YOLO("yolov8s.pt")
+# Kullanıcının model seçimi
+model_option = st.selectbox(
+    "Bir YOLO modeli seçin:",
+    ["YOLOv9 Small (yolov9s.pt)", "YOLOv10 Small (yolov10s.pt)", "YOLO11 Small (yolo11s.pt)"]
+)
 
-model = load_model()
+# Seçilen modele göre yükleme
+@st.cache_resource  
+def load_model(model_name):
+    return YOLO(model_name)
+
+model_dict = {
+    "YOLOv8 Small (yolov8s.pt)": "yolov9s.pt",
+    "YOLOv8 Medium (yolov8m.pt)": "yolov10s.pt",
+    "YOLOv8 Large (yolov8l.pt)": "yolo11s.pt"
+}
+
+model = load_model(model_dict[model_option])
 
 uploaded_file = st.file_uploader("Bir PNG dosyası yükleyin", type=["png"])
 
@@ -19,25 +32,7 @@ if uploaded_file is not None:
     # Nesne tespiti yap
     st.write("### Nesne Tespiti Yapılıyor...")
     results = model.predict(image)
-    boxes = results.boxes
-    labels = results.names
-    # labels, her nesne için bir sayıyı verir. Örneğin, [0, 1, 2] gibi.
-
-    # Tanınan nesneleri ve sayısını saymak için:
-    object_counts = {}
-
-    for label in boxes.cls:
-        # labels'dan nesne etiketine dönüştür
-        label_name = labels[int(label)]
-        if label_name not in object_counts:
-            object_counts[label_name] = 1
-        else:
-            object_counts[label_name] += 1
     
     # Tespit edilen nesneleri göster
     result_image = results[0].plot()
     st.image(result_image, caption='Tespit Sonucu', use_column_width=True)
-
-    st.write("Detected objects and their counts:")
-    for obj, count in object_counts.items():
-        st.write(f"{obj}: {count}")
