5df333a570ca586bc003f4c573996a287a5ef37b
HasanCeo <125748600+HasanCeo@users.noreply.github.com>
2024-11-28 14:04:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from ultralytics import YOLO
 
 st.title("YOLO Object Detection Application")
 
-# Model selection by the user
+
 model_option = st.selectbox(
     "Choose a YOLO model:",
     ["YOLO9s", "YOLO10s", "YOLO11s"]
 )
 
-# Load the selected model
 @st.cache_resource
 def load_model(model_name):
     return YOLO(model_name)
@@ -23,17 +22,17 @@ model_files = {
 
 model = load_model(model_files[model_option])
 
-# Image upload section
+
 uploaded_file = st.file_uploader("Upload a PNG file", type=["png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    # Perform object detection
+
     st.write("### Performing Object Detection...")
     results = model.predict(image)
     
-    # Display results
-    result_image = results[0].plot()  # Annotated image
+  
+    result_image = results[0].plot()  
     st.image(result_image, caption='Detection Result', use_column_width=True)
